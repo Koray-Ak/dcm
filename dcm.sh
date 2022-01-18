e04a92986dd731dc1ae075258f485423807eccc6
root <root@ansible.bash0.com>
2022-01-18 16:16:49 +0300

    dcm developed

---
+++ dcm.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+help=$1
+ssl=$1
+ps=$1
+inspect=$1
+ip=$1
+mount=$1
+network=$1
+version=$1
+
+case $help in
+"help" )
+echo " About running Docker information"
+echo " dcm ps :  List all Container given Host"
+echo " dcm version : Show the Docker version information"
+echo " dcm version -f : Format the output using the given Go template "
+echo ""
+echo " dcm inspect : dcm inspect provides detailed information on constructs controlled by Docker"
+echo " dcm inspect help : Details for usage"
+echo ""
+echo " dcm ip [Driver Name] [Contaniner Name] : You can use which container are using Network Driver with dcm network command."
+echo " dcm mount [Container Name] : The command shows container Mount Details"
+echo " dcm ssl [Domain Name] : The command extend SSL Expire date"
+;;
+* );;
+esac
+
+case $version in
+"version" )
+if [ "$2" = "-f" ] ; then
+docker version --format "{{${3}}}"
+elif [ "$1" = "version" ] ; then
+docker version
+fi
+
+;;
+
+*);;
+esac
+
+case $ssl in
+"ssl" )
+email="peter.korduan@gdi-service.de"
+webroot=/var/www/html
+path=/home/gisadmin/networks/proxy/services/proxy
+docker run --rm --interactive --name certbot \
+    -v "$path/www/html:/var/www/html" \
+    -v "$path/letsencrypt:/etc/letsencrypt" \
+    -v "$path/log:/var/log/nginx" \
+certbot/certbot certonly --webroot -w $webroot -d $2 --email $email
+;;
+* );;
+esac
+
+case $ps in
+"ps" )
+docker ps -a ;;
+* );;
+esac
+
+case $inspect in
+"inspect" )
+
+if [ "$2" = "help" ] ; then
+	echo ""
+	echo "dcm inspect [Container Name] [Options]"
+	echo ""
+	echo " - Mounts"
+	echo " - Config"
+	echo "   - .Hostname"
+	echo "   - .Domainname"
+	echo "   - .Env"
+        echo "   - .Cmd"
+	echo "   - .Image"
+        echo "   - .Labels"
+	echo " - NetworkSettings"
+	echo " - NetworkSettings.Networks"
+	echo "   - .[Network Name].IPAddress   [ You can search which container are using which network driver command : dcm network ls ]"
+	echo "   - .[Network Name].Gateway"
+	echo "   - .[Network Name].Aliases"
+	echo "   - .[Network Name].MacAddress"
+	echo ""
+elif [ "$1" = "inspect" ] ; then
+echo "Usage for details command dcm inspect help"
+
+else
+docker inspect $2 --format "{{json .${3}}}" | jq -r $4
+fi;;
+* );;
+esac
+
+
+
+
+
+case $ip in
+"ip" )
+docker inspect $3 --format "{{json .NetworkSettings.Networks}}" | jq -r ".$2.IPAddress"
+;;
+* );;
+esac
+
+
+case $mount in
+"mount" )
+docker inspect $2 --format "{{json .Mounts}}" | jq ;;
+* );;
+esac
+
+case $network in
+"network" )
+if [ "$2" = "ls" ] ; then
+docker ps --format "table{{.Names}}\t{{.Networks}}"
+else
+exit
+fi ;;
+
+* );;
+esac
